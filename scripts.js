37a166037145b9242783f9d2ea1167c28a869473
yvm123 <amutsinzi@gmail.com>
2020-12-14 18:08:39 +0200

    initial commit

--- scripts.js
+++ scripts.js
@@ -3,3 +3,14 @@ function submit() {
     var day = new Date(document.getElementById("bday").value);
     var d = day.getDay();
     var m= day.getMonth();
+    var maleNames=['Kwasi',"Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+    var femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+    var n=0;
